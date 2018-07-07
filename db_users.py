a673c3924c36e748d06ec2dcd69366e39d30e562
unknown <tjasaa@gmail.com>
2018-07-07 12:08:35 +0200

    get user_id, update validation

--- db_users.py
+++ db_users.py
@@ -3,7 +3,7 @@ from cnxn import db_query, db_write
 # Check if username and password exist in DB for login
 def check_username_password(username, password):
     data = db_query("SELECT username, password FROM data_User WHERE username=? AND password=?;", username, password)
-    print(data)
+    # print(data)
     return True if data else False
     
 # Check if username dont exist yet for registration
@@ -15,3 +15,9 @@ def check_username_dont_exists(username):
 def add_new_user_to_db(first, last, username, password):
     db_write("INSERT INTO data_User(first, last, username, password) VALUES (?, ?, ?, ?);", first, last, username, password)
     return
+
+# Get user_id for username
+def get_user_id(username):
+    data = db_query("SELECT user_id FROM data_User WHERE username=?;", username)
+    # print(data)
+    return data
--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ from wtforms import Form, StringField, PasswordField, validators
 
 # Validation of fields for user registration
 class RegistrationForm(Form):
-    first = StringField('first', [validators.DataRequired(), validators.Length(min=2, max=25)])
-    last = StringField('last', [validators.DataRequired(), validators.Length(min=2, max=25)])
-    username = StringField('username', [validators.DataRequired(), validators.Length(min=4, max=25)])
+    first = StringField('first', [validators.DataRequired(), validators.Length(min=3, max=25)])
+    last = StringField('last', [validators.DataRequired(), validators.Length(min=3, max=25)])
+    username = StringField('username', [validators.DataRequired(), validators.Length(min=3, max=25)])
     password = PasswordField('password', [validators.DataRequired(), validators.Length(min=4, max=25)])

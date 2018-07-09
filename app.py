81bc04ba98f8ee98ce303bea50fdacb4b3d5cbcb
unknown <tjasaa@gmail.com>
2018-07-09 19:26:24 +0200

    update functions, simplify

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from validate import RegistrationForm
 import fractions
-from natsort import natsorted
-from operator import itemgetter
 from datetime import datetime
+import re
 import uuid
+import helpers
 import db_users
 import db_recipes
 import db_recipe_add
@@ -16,17 +16,14 @@ app.secret_key = "fhkjashgdfkhakjdfgkjasdgf"
 @app.route('/')
 def index():
     # If user is logged in, logout button will be displayed
-    if 'username' in session:
-        username = session['username']
-    else:
-        username = None
+    username = helpers.username_set_or_none()
     return render_template('index.html', username=username)
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
         # If entered data exist in DB, user will be logged in else error message is displayed
         login_data = db_users.check_username_password(username, password)
         print(login_data)
@@ -50,10 +47,10 @@ def register():
     form = RegistrationForm(request.form)
     # Check if fields are valid with validate() method
     if request.method == 'POST' and form.validate():
-        first = form.first.data.title()
-        last = form.last.data.title()
-        username = form.username.data
-        password = form.password.data
+        first = form.first.data.strip().title()
+        last = form.last.data.strip().title()
+        username = form.username.data.strip()
+        password = form.password.data.strip()
         # Check if name contains only alpha characters
         if first.isalpha() and last.isalpha():
             # Check DB if username is availible
@@ -96,138 +93,125 @@ def add_recipe():
         username = session['username']
     else:
         return redirect(url_for('login'))
-    
-    # Create random recipe_id
-    recipe_id = uuid.uuid4()
-
-    # Get data for dropdown selection from DB, sort numerical values
-    types = db_recipes.get_food_types()
-    servings_data = db_recipes.get_number_of_servings()
-    servings = []
-    for entry in natsorted(servings_data, key=itemgetter(1), reverse=False):
-        servings.append(entry)
-    minutes_data = db_recipes.get_preparation_time()
-    minutes = []
-    for entry in natsorted(minutes_data, key=itemgetter(1), reverse=False):
-        minutes.append(entry)
-    if request.method == 'POST':
-        """ Get data from form """
         
-    # Get all ingredients data from form - list of sorted tuples
+    # Get data for dropdown selection from DB, sort numerical values
+    data = db_recipes.get_types_servings_time()
+    types = data[0]
+    servings = []
+    minutes = []
+    helpers.sort_numbers(data[1], servings)
+    helpers.sort_numbers(data[2], minutes)
+    
+    return render_template('recipe_add.html', username=username, types=types, n_servings=servings, n_minutes=minutes)
+
+@app.route('/account/addrecipe/check_data', methods=['GET', 'POST'])
+def get_check_data():
+    if request.method == 'POST':
+    # Create random recipe_id, get user_id converted to UUID, other data
+        recipe_id = uuid.uuid4()
+        user_id = uuid.UUID(session['user_id'])
+        creation_date = datetime.now()
+        title = helpers.remove_whitespaces(request.form['title']).strip().title()
+        description = request.form['description']
+        image = request.form['image'].strip()
+        steps = request.form['steps']
+        
+    # Get IDs of selected dropdown data, converted to UUIDs
+        type_id = uuid.UUID(request.form.get('type_option'))
+        servings_id = uuid.UUID(request.form.get('servings_option'))
+        time_id = uuid.UUID(request.form.get('minutes_option'))
+
+    # Get all ingredients data from form
         quantities = request.form.getlist('quantity[]')
         quantities.append(request.form['quantity'])
-        q_list = []
-        for q in quantities:
-            q2 = q.replace(",", ".")
-            q3 = float(q2)
-            q_list.append(q3)
         units = request.form.getlist('unit[]')
         units.append(request.form['unit'])
         ingredients = request.form.getlist('ingredient[]')
         ingredients.append(request.form['ingredient'])
-        
-        input_data = zip(q_list, units, ingredients)
-        formatted_input_data = list(input_data)
-        print(formatted_input_data)
-    # Check if ingredients data exists in DB
-        for entry in formatted_input_data:
-            quantity = entry[0]
-            unit = entry[1]
-            ingredient = entry[2]
-            q_data = db_recipe_add.check_if_quantity_in_db(quantity)
-            u_data = db_recipe_add.check_if_unit_in_db(unit)
-            i_data = db_recipe_add.check_if_ingredient_in_db(ingredient)
-            print(q_data, u_data, i_data)
-            
-        ### 3 lists: quantity_ids, unit_ids and ingredient_ids; zip them and add each tuple + recipe_id to data_Ingredients
-        # If ingredients data exists, get their ID, else create them and add new record to each dft_table
-            quantity_ids = []
+
+        # IDs for entered data
+        quantity_ids = []
+        unit_ids = []
+        ingredient_ids = []
+    # Check type and format quantity, check if ingredients data exists in DB
+        for q in quantities:
+            q2 = q.replace(",", ".")
+            if helpers.isFloat(q2):
+                q = float(q2)
+            else:
+                q = float('0')
+            q_data = db_recipe_add.check_if_quantity_in_db(q)
+        # If quantity data exists in DB, get their ID, else create it and add new record to dft_tablee
             if q_data:
-                q_id_data = db_recipe_add.get_quantity_id(quantity)
+                q_id_data = db_recipe_add.get_quantity_id(q)
                 for data in q_id_data:
-                    quantity_id = data.quantity_id
-                    quantity_ids.append(quantity_id)
+                    q_id = data.quantity_id
+                    quantity_ids.append(q_id)
             else:
-                quantity_id = uuid.uuid4()
-                quantity_ids.append(quantity_id)
-                db_recipe_add.add_new_quantity(quantity_id, quantity)
-                
-            unit_ids = []
+                q_id = uuid.uuid4()
+                quantity_ids.append(q_id)
+                db_recipe_add.add_new_quantity(q_id, q)
+    # If units data exists in DB, get their ID, else create it and add new record to dft_table
+        for u in units:
+            u = u.strip().lower()
+            u_data = db_recipe_add.check_if_unit_in_db(u)
+            print(u_data)
             if u_data:
-                u_id_data = db_recipe_add.get_unit_id(unit)
+                u_id_data = db_recipe_add.get_unit_id(u)
                 for data in u_id_data:
-                    unit_id = data.unit_id
-                    unit_ids.append(unit_id)
+                    u_id = data.unit_id
+                    unit_ids.append(u_id)
             else:
-                unit_id = uuid.uuid4()
-                unit_ids.append(unit_id)
-                db_recipe_add.add_new_unit(unit_id, unit)
-                
-            ingredient_ids = []
+                u_id = uuid.uuid4()
+                unit_ids.append(u_id)
+                db_recipe_add.add_new_unit(u_id, u)
+    # If ingredients data exists in DB, get their ID, else create it and add new record to dft_table
+        for i in ingredients:
+            i = i.strip().lower()
+            i_data = db_recipe_add.check_if_ingredient_in_db(i)
+            print(i_data)
             if i_data:
-                i_id_data = db_recipe_add.get_ingredient_id(ingredient)
+                i_id_data = db_recipe_add.get_ingredient_id(i)
                 for data in i_id_data:
-                    ingredient_id = data.ingredient_id
-                    ingredient_ids.append(ingredient_id)
+                    i_id = data.ingredient_id
+                    ingredient_ids.append(i_id)
             else:
-                ingredient_id = uuid.uuid4()
-                ingredient_ids.append(ingredient_id)
-                db_recipe_add.add_new_ingredient(ingredient_id, ingredient)
-            
-            # Zip lists and add add records to data_Ingredients
-            for i,u,q in zip(ingredient_ids, unit_ids, quantity_ids):
+                i_id = uuid.uuid4()
+                ingredient_ids.append(i_id)
+                db_recipe_add.add_new_ingredient(i_id, i)
+
+
+    # Merge lists with IDs and add each group + recipe_id to data_Ingredients
+        for i,u,q in zip(ingredient_ids, unit_ids, quantity_ids):
                 db_recipe_add.add_new_ingredient_to_db(i, u, q, recipe_id)
-            
-            # zipped_ingredients = zip(ingredient_ids, unit_ids, quantity_ids)
-            # list_ingredients = list(zipped_ingredients)
-            # for i, u, q in zipped_ingredients:
-            #     db_recipe_add.add_new_ingredient_to_db(i.ingredient_id, u.unit_id, q.quantity_id, recipe_id)
     
-    
-    # Get IDs of selected dropdown data, converted to UUIDs
-        type_selected = uuid.UUID(request.form.get('type_option'))
-        servings_selected = uuid.UUID(request.form.get('servings_option'))
-        minutes_selected = uuid.UUID(request.form.get('minutes_option'))
-        
-    # Get all other recipe data
-        title = request.form['title']
-        description = request.form['description']
-        image = request.form['image']
-        steps = request.form['steps']
-
-    # Get user_id converted to UUID, creation_date
-        user_id = uuid.UUID(session['user_id'])
-        creation_date = datetime.now()
-
     # Add data to data_Recipe and redirect to details page
-        db_recipe_add.add_new_recipe_to_db(recipe_id, title, description, steps, creation_date, image, user_id, type_selected, servings_selected, minutes_selected)
+        db_recipe_add.add_new_recipe_to_db(recipe_id, title, description, steps, creation_date, image, user_id, type_id, servings_id, time_id)
+
+
         return redirect(url_for('recipe_details', recipe_id=recipe_id))
-    
-    return render_template('recipe_add.html', username=username, types=types, n_servings=servings, n_minutes=minutes)
+
+    return
+
 
 @app.route('/recipes')
 def recipes():
-    if 'username' in session:
-        username = session['username']
-    else:
-        username = None
+    username = helpers.username_set_or_none()
+    
     # Get basic info for all recipes
-    recipes = db_recipes.recipe_short()
-    for entry in recipes:
-        description = entry.description.split("\r")
-    return render_template('recipes.html', username=username, recipes=recipes, description=description)
+    data = db_recipes.recipe_short()
+    for entry in data:
+        entry.description = entry.description.split("\r")
+    return render_template('recipes.html', username=username, data=data)
 
 @app.route('/recipes/details/<recipe_id>')
-def recipe_details(recipe_id):
-    if 'username' in session:
-        username = session['username']
-    else:
-        username = None
+def recipe_details(recipe_id):  
+    username = helpers.username_set_or_none()
     # Get all recipe data except ingredients
     data = db_recipes.recipe_details_description(recipe_id)
     # Split description into steps
     for entry in data:
-        f_steps = entry.steps.split("\r")
+        f_steps = re.split(': |\r|\n', entry.steps)
         entry.steps = f_steps
         f_description = entry.description.split("\r")
         entry.description = f_description

0d9178532e3ddf9d3ec053bf185ee2b8da30de68
TJASA <tjasaa@gmail.com>
2018-08-16 16:32:00 +0200

    add file with DB connection info

---
+++ creds.py
@@ -0,0 +1,6 @@
+server = 'auxdev.intech.si'
+database = 'TCI'
+username = 'Tjasa'
+password = 'tjasabaza111'
+
+conn_str = 'DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password;
